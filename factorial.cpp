4f48c0e2e72bf2c31ae8e423393104ddae4c846d
RyanVillenaUCR <rvill028@ucr.edu>
2020-02-03 00:57:03 -0800

    Adapt to use CLI input

--- factorial.cpp
+++ factorial.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 int factorial(int n) {
 
-	int i = n;
-	for (n--; n >= 0; --n)
-		i *= n;
-	return i;
+    int i = n;
+    for (n--; n >= 0; --n)
+        i *= n;
+    return i;
 }
 
-int main() {
-	
-	std::cout << "Running factorial..." << std::endl;
+int main(int argc, char* argv[]) {
+    
+    std::cout << "Running factorial..." << std::endl;
 
-	int n = 4;
+    const int DEFAULT_VALUE = 4;
+    int n = (argc == 2 ? atoi(argv[1]) : DEFAULT_VALUE); // Prioritize CLI input, else hardcode value
 
-	std::cout << "factorial(" << n << "): "
-	          << factorial(n) << std::endl;
+    std::cout << "factorial(" << n << "): "
+              << factorial(n) << std::endl;
 }
